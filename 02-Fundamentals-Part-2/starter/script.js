a6e36ce3a6a63aead21bbcb35ab043b8ee15e0bd
Ona <onushkis@gmail.com>
2021-11-14 21:54:50 +0100

    add changes funtions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,8 +1,36 @@
 "use strict";
+
+// ---------------------------------Activating Strik Mode
 let hasDrivingLincence = false;
 const passTest = true;
 if (passTest) hasDrivingLincence = true;
 
 if (hasDrivingLincence) console.log("I can drive");
 
-const interface = 'Audio';
+// const interface = 'Audio';
+
+
+// ---------------------------------Funktions
+
+function logger(){
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking funktion
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    // console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return(juice);
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+ const num = Number('23');
--- Part2/index.js
+++ Part2/index.js
@@ -1,4 +1,6 @@
 "use strict";
+
+// ---------------------------------Activating Strik Mode
 let hasDrivingLincence = false;
 const passTest = true;
 if (passTest) hasDrivingLincence = true;
@@ -6,3 +8,29 @@ if (passTest) hasDrivingLincence = true;
 if (hasDrivingLincence) console.log("I can drive");
 
 // const interface = 'Audio';
+
+
+// ---------------------------------Funktions
+
+function logger(){
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking funktion
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges){
+    // console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return(juice);
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+ const num = Number('23');
